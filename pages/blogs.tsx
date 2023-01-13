bc5d5d3287c64e7519b02f342e54f253a421c3e0
JoaoPeSousa <Starwars9304>
2023-01-13 15:23:51 -0300

    Pages + Chakra UI

---
+++ pages/blogs.tsx
@@ -0,0 +1,8 @@
+const Blogs = () => {
+    return (
+        <h1>I am Blogs page</h1>
+    )
+}
+
+
+export default Blogs
---
+++ pages/cv.tsx
@@ -0,0 +1,8 @@
+const Cv = () => {
+    return (
+        <h1>I am cv page</h1>
+    )
+}
+
+
+export default Cv
